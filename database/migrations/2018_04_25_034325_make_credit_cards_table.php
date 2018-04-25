f6e79563c038c32257fee4b8f925e59707f6b915
Sam Wilson <swilsondev@outlook.com>
2018-04-25 00:49:59 -0500

    Writing migrations for customers, credit cards, addresses, products, and categories. [TL#2]

---
+++ database/migrations/2018_04_25_034325_make_credit_cards_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeCreditCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credit_cards', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('number');
+            $table->unsignedInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('credit_cards');
+    }
+}
